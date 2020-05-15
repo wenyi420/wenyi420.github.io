82bac8f22ef659064aa2e88704aab014f81638bd
wenyi420 <t0987320406@gmail.com>
2020-05-15 21:10:59 +0800

    修正 TodoList.vue

--- vuetodolist/src/components/TodoList.vue
+++ vuetodolist/src/components/TodoList.vue
@@ -93,23 +93,7 @@
       }
     }
   }
-  .bounce-enter-active {
-    animation: bounce-in 0.5s;
-  }
-  .bounce-leave-active {
-    animation: bounce-in 0.5s reverse;
-  }
-  @keyframes bounce-in {
-    0% {
-      transform: scale(0);
-    }
-    50% {
-      transform: scale(1.5);
-    }
-    100% {
-      transform: scale(1);
-    }
-  }
+
   @media screen and (max-width: 480px) {
     .todo-list .tabs span {
       padding: 8px 13px;
