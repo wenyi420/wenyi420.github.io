835914a290a40bf71d9c42cf10038d059616e1ba
wenyi420 <t0987320406@gmail.com>
2020-05-25 21:33:13 +0800

    修改後台

--- shoppingcart/src/components/orderListItem.vue
+++ shoppingcart/src/components/orderListItem.vue
@@ -24,157 +24,224 @@
     <div class="item-col item-col-2 payStatus">
       <label class="title">狀態</label>
       <p class="content">
-        <span v-if="item.isPay" class="text-success">已付款</span>
-        <span v-else class="text-danger">未付款</span>
+        <span
+          v-if="item.isPay"
+          class="text-success"
+        >已付款</span>
+        <span
+          v-else
+          class="text-danger"
+        >未付款</span>
       </p>
     </div>
     <div class="item-col item-col-2 edit">
       <label class="title">編輯訂單</label>
       <p class="content">
-        <span class="btn edit" @click="goEditPage">詳細</span>
+        <span
+          class="btn edit"
+          @click="goEditPage"
+        >詳細</span>
       </p>
     </div>
     <div class="item-col item-col-2 delete">
       <label class="title">刪除訂單</label>
       <p class="content">
-        <span class="btn delete">刪除</span>
+        <span
+          class="btn delete"
+          @click="deleteOrder"
+        >刪除</span>
       </p>
     </div>
   </div>
 </template>
 <script>
-export default {
-  name: "orderListItem",
-  data() {
-    return {};
-  },
-  props: {
-    item: {
-      type: Object,
-      require: true,
-    },
-  },
+  import { Toast } from "vant";
 
-  methods: {
-    goEditPage() {
-      this.$router.push(`/admin/editOrder/${this.item.id}`);
+  export default {
+    name: "orderListItem",
+    data() {
+      return {};
     },
-  },
-};
+    props: {
+      item: {
+        type: Object,
+        require: true
+      }
+    },
+
+    methods: {
+      goEditPage() {
+        this.$router.push(`/admin/editOrder/${this.item.id}`);
+      },
+      deleteOrder() {
+        // 請求攔截器添加
+        let detailInter = this.$axios.create({});
+        detailInter.interceptors.request.use(
+          config => {
+            // 發起請求前執行什麼
+            Toast.loading({
+              mask: false,
+              duration: 0, // 一直存在
+              forbidClick: true, //禁止點擊
+              message: "加載中..."
+            });
+            return config;
+          },
+          err => {
+            // 請求錯誤
+            Toast.clear();
+            Toast("請求錯誤, 請稍後重試");
+            console.log(err);
+          }
+        );
+
+        // 響應攔截器
+        detailInter.interceptors.response.use(
+          res => {
+            // 請求成功
+            Toast.loading({
+              mask: false,
+              duration: 1, // 一直存在
+              forbidClick: true, //禁止點擊
+              message: "刪除成功"
+            });
+            return res;
+          },
+          err => {
+            // 請求錯誤
+            Toast.clear();
+            Toast("請求錯誤, 請稍後重試");
+            console.log(err);
+          }
+        );
+        let id = this.item.id;
+
+        detailInter
+          .delete(`https://vueshopcart.herokuapp.com/orderlist/${id}`)
+          .then(res => {
+            this.$emit("UPDATE_ORDERLIST", id);
+            console.log("刪除成功", res);
+          })
+          .catch(err => {
+            console.log("刪除失敗", err);
+          });
+      }
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
-.item-col-1 {
-  width: 5%;
-}
-.item-col-2 {
-  width: 10%;
-}
-.item-col-3 {
-  width: 15%;
-}
-.item-col-4 {
-  width: 25%;
-}
-.item-col-5 {
-  width: 25%;
-}
-.item-col-6 {
-  width: 30%;
-}
+  .item-col-1 {
+    width: 5%;
+  }
+  .item-col-2 {
+    width: 10%;
+  }
+  .item-col-3 {
+    width: 15%;
+  }
+  .item-col-4 {
+    width: 25%;
+  }
+  .item-col-5 {
+    width: 25%;
+  }
+  .item-col-6 {
+    width: 30%;
+  }
 
-.orderList {
-  display: flex;
-  flex-wrap: wrap;
-  .item {
+  .orderList {
     display: flex;
-    width: 100%;
-    justify-content: center;
-    .item-col {
-      .title {
-        background: #969799;
-        display: block;
-        width: 100%;
-        padding: 10px 0px;
-        color: #fff;
-        /* margin-bottom: 15px; */
-        margin: 0px;
-      }
-      .content {
-        margin: 0px;
-        width: 100%;
-        padding: 10px 0px;
-        .btn {
+    flex-wrap: wrap;
+    .item {
+      display: flex;
+      width: 100%;
+      justify-content: center;
+      .item-col {
+        .title {
+          background: #969799;
           display: block;
-          padding: 5px;
-          background: #42b983;
-          width: 75%;
-          margin: 0 auto;
-          border-radius: 5px;
+          width: 100%;
+          padding: 10px 0px;
           color: #fff;
-          opacity: 0.8;
-          transition: ease 0.3s;
-          cursor: pointer;
-          font-size: 14px;
-          &:hover {
-            opacity: 1;
-          }
-          &.delete {
-            background: red;
+          /* margin-bottom: 15px; */
+          margin: 0px;
+        }
+        .content {
+          margin: 0px;
+          width: 100%;
+          padding: 10px 0px;
+          .btn {
+            display: block;
+            padding: 5px;
+            background: #42b983;
+            width: 75%;
+            margin: 0 auto;
+            border-radius: 5px;
+            color: #fff;
+            opacity: 0.8;
+            transition: ease 0.3s;
+            cursor: pointer;
+            font-size: 14px;
+            &:hover {
+              opacity: 1;
+            }
+            &.delete {
+              background: red;
+            }
           }
         }
       }
-    }
-    .item-col + .item-.col {
-      border-left: 1px solid #eee;
-    }
-    .movie-item {
-      padding-bottom: 15px;
+      .item-col + .item-.col {
+        border-left: 1px solid #eee;
+      }
+      .movie-item {
+        padding-bottom: 15px;
 
-      .content {
-        padding: 0px 10px 7px;
-        text-align: left;
+        .content {
+          padding: 0px 10px 7px;
+          text-align: left;
+        }
+        .content + .content {
+          border-top: 1px solid #eee;
+          padding-top: 7px;
+        }
       }
-      .content + .content {
-        border-top: 1px solid #eee;
-        padding-top: 7px;
+    }
+    .item + .item {
+      padding-top: 5px;
+      .title {
+        display: none;
       }
+    }
+    .item:nth-child(2n) {
+      background: #eee;
     }
   }
-  .item + .item {
-    padding-top: 5px;
-    .title {
+  @media screen and (min-width: 769px) and (max-width: 920px) {
+    .item-col-2 {
+      width: 15%;
+    }
+    .orderList .item .email {
       display: none;
     }
   }
-  .item:nth-child(2n) {
-    background: #eee;
-  }
-}
-@media screen and (min-width: 769px) and (max-width: 920px) {
-  .item-col-2 {
-    width: 15%;
-  }
-  .orderList .item .email {
-    display: none;
-  }
-}
-@media screen and (max-width: 768px) {
-  .orderList .item {
-    flex-wrap: wrap;
-    .item-col {
-      display: flex;
-      width: 100%;
+  @media screen and (max-width: 768px) {
+    .orderList .item {
+      flex-wrap: wrap;
+      .item-col {
+        display: flex;
+        width: 100%;
+        .title {
+          width: 120px;
+        }
+      }
+    }
+    .orderList .item + .item {
+      padding-top: 0px;
       .title {
-        width: 120px;
+        display: block;
       }
     }
   }
-  .orderList .item + .item {
-    padding-top: 0px;
-    .title {
-      display: block;
-    }
-  }
-}
 </style>
--- shoppingcart/src/views/orderList.vue
+++ shoppingcart/src/views/orderList.vue
@@ -6,46 +6,94 @@
         v-for="item in orderListData"
         :item="item"
         :key="item.id"
+        @UPDATE_ORDERLIST="updateOrderList"
       />
     </div>
   </div>
 </template>
 <script>
-import { Dialog } from "vant";
-import orderListItem from "@/components/orderListItem.vue";
-export default {
-  name: "orderList",
-  beforeCreate() {
-    let token = localStorage.getItem("Authorization");
-    if (!token) {
-      Dialog({ message: "請登入後台" });
-      this.$router.push("/login");
+  import { Dialog } from "vant";
+  import { Toast } from "vant";
+
+  import orderListItem from "@/components/orderListItem.vue";
+  export default {
+    name: "orderList",
+    beforeCreate() {
+      let token = localStorage.getItem("Authorization");
+      if (!token) {
+        Dialog({ message: "請登入後台" });
+        this.$router.push("/login");
+      }
+    },
+    data() {
+      return {
+        orderListData: []
+      };
+    },
+    components: {
+      orderListItem
+    },
+    mounted() {
+      // 請求攔截器添加
+      let detailInter = this.$axios.create({});
+      detailInter.interceptors.request.use(
+        config => {
+          // 發起請求前執行什麼
+          Toast.loading({
+            mask: false,
+            duration: 0, // 一直存在
+            forbidClick: true, //禁止點擊
+            message: "加載中..."
+          });
+          return config;
+        },
+        err => {
+          // 請求錯誤
+          Toast.clear();
+          Toast("請求錯誤, 請稍後重試");
+          console.log(err);
+        }
+      );
+
+      // 響應攔截器
+      detailInter.interceptors.response.use(
+        res => {
+          // 請求成功
+          Toast.clear();
+          return res;
+        },
+        err => {
+          // 請求錯誤
+          Toast.clear();
+          Toast("請求錯誤, 請稍後重試");
+          console.log(err);
+        }
+      );
+      console.log(1);
+      detailInter
+        .get("https://vueshopcart.herokuapp.com/orderlist")
+        .then(res => {
+          this.orderListData = res.data;
+        })
+        .catcg(err => {
+          console.log(err);
+        });
+    },
+    methods: {
+      updateOrderList(id) {
+        let index = this.orderListData
+          .map(item => {
+            return item.id;
+          })
+          .findIndex(item => item === id);
+        this.orderListData.splice(index, 1);
+      }
     }
-  },
-  data() {
-    return {
-      orderListData: [],
-    };
-  },
-  components: {
-    orderListItem,
-  },
-  mounted() {
-    console.log(1);
-    this.$axios
-      .get("https://vueshopcart.herokuapp.com/orderlist")
-      .then((res) => {
-        this.orderListData = res.data;
-      })
-      .catcg((err) => {
-        console.log(err);
-      });
-  },
-};
+  };
 </script>
 <style lang="scss">
-.orderList {
-  border: 1px solid #969799;
-  margin-top: 30px;
-}
+  .orderList {
+    border: 1px solid #969799;
+    margin-top: 30px;
+  }
 </style>
