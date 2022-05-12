f3a411ef8c088198094bf1b708eb230c50dc511b
wenyi420 <t0987320406@gmail.com>
2022-05-12 23:20:58 +0800

    [FEAT] 補上打包檔案

--- movies/dist/assets/AboutView.53409d85.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,f as _}from"./index.87a49207.js";const o={},a={class:"about"};function c(r,s){return t(),_("div",a,"\u6E2C\u8A66 about")}var u=e(o,[["render",c]]);export{u as default};
